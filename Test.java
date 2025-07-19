963e2b0a8807fada6c800cb7be6ec431eedbef2b
vallipedu <harishankarmutchakala@gmail.com>
2025-07-19 20:36:55 +0530

    hello

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+        //sample code to demonstrate java ternary operator
+        int a = 10;
+        int b = 20;
+        int max = (a > b) ? a : b;
+        System.out.println("The maximum value is: " + max);
+        //sample code to demonstrate java for loop
+        for (int i = 0; i < 5; i++) {   
+            System.out.println("Current value of i: " + i);       
+        }
+            }
+}
